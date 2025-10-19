23dd708e3a33d9ac67e0f8cf8b7ca5b8e8842097
Yuri Panchul <yuri@panchul.com>
2025-10-19 09:12:40 -0700

    Debugging the first commit

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,8 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // tt_um_example user_project (
+  tt_um_verilog_meetup_template_project_TODO user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
